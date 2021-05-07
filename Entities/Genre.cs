83e11944262057cd33476aa654b815db110d9947
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-05-07 10:09:44 +0100

    Custom Validation: Entity

--- Entities/Genre.cs
+++ Entities/Genre.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using MoviesAPI.Validations;
 
 namespace MoviesAPI.Entities
 {
@@ -6,6 +7,7 @@ namespace MoviesAPI.Entities
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "The feild ' {0} ' is required")]
+        [FirstLetterUppercase]
         public string Name { get; set; }
     }
 }
