f9039e830236793a862d48185e36f8f5fa1dab32
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-05-15 13:48:05 +0100

    Adding Actor Entity
    Adding actor to the database using migrations, also creating the controller with the functions

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public ActorsController(ApplicationDbContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDTO>>> Get()
+        {
+            var actors = await _ctx.Actors.OrderBy(x => x.Name).ToListAsync();
+            return _mapper.Map<List<ActorDTO>>(actors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ActorDTO>> Get(int id)
+        {
+            var actors = await _ctx.Actors.FirstOrDefaultAsync(x => x.Id == id);
+            if (actors == null)
+            {
+                return NotFound();
+            }
+            return _mapper.Map<ActorDTO>(actors);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ActorCreationDTO actorCreationDTO)
+        {
+            var actor = _mapper.Map<Actor>(actorCreationDTO);
+            _ctx.Add(actor);
+            await _ctx.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ActorCreationDTO actorCreationDTO)
+        {
+            var actor = _mapper.Map<Actor>(actorCreationDTO);
+            actor.Id = id;
+            _ctx.Entry(actor).State = EntityState.Modified;
+            await _ctx.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var actor = await _ctx.Actors.FirstOrDefaultAsync(x => x.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            _ctx.Remove(actor);
+            await _ctx.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
