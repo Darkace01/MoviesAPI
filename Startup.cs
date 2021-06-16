778627725e1a8e85a5aa2dedcc628f1a669757e6
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-06-16 14:02:53 +0100

    Enable swagger in production

--- Startup.cs
+++ Startup.cs
@@ -115,12 +115,12 @@ namespace MoviesAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> _logger)
         {
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
+                
             }
 
             app.UseHttpsRedirection();
